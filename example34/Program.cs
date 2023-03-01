ae73daf8ccbe22f6806e230c4b303de90954f6ae
JuliaRomaeva <julia.romaeva@gmail.com>
2023-03-01 16:32:43 +0300

    количество четных чисел в заданном массиве из n элементов

---
+++ example34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный
+// случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+Console.Write("Введите длину массива трехзначных чисел: ");
+int num = int.Parse(Console.ReadLine());
+
+int[] array = FillArray(num, 100, 1000);
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i =0; i < size; i++)
+    {
+        result[i] = new Random().Next(min,max);
+    }
+    return result;
+}
+
+int CountEvenNumbers = 0;
+for(int i = 0; i < array.Length; i++){
+    int el = array[i];
+    if(el % 2 == 0) 
+    CountEvenNumbers = CountEvenNumbers + 1;    
+}
+
+Console.Write($"[{String.Join(", ", array)}] -> {CountEvenNumbers}");
